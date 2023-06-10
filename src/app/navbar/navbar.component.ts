e3797fa63a47f8a6f832adf3b3e80dbfb835659e
abhijithmahipal <abhijithmahipal@gmail.com>
2023-06-10 19:18:58 +0530

    feat: added basic layout

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss'],
+})
+export class NavbarComponent {
+  isMobileMenuOpen: boolean = false;
+
+  constructor(private router: Router) {}
+
+  toggleMobileMenu(): void {
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;
+  }
+
+  closeMobileMenu(): void {
+    this.isMobileMenuOpen = false;
+  }
+
+  navigateTo(route: string): void {
+    this.router.navigate([route]);
+  }
+}
